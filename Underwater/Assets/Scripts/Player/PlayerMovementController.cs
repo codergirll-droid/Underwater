adffbedde89084d85b88367ff7752af6eadf8d30
Ayşe Oduncu <oduncu981@gmail.com>
2022-03-07 01:29:04 +0300

    updated player movement

--- Underwater/Assets/Scripts/Player/PlayerMovementController.cs
+++ Underwater/Assets/Scripts/Player/PlayerMovementController.cs
@@ -7,15 +7,46 @@ public class PlayerMovementController : MonoBehaviour
 {
 
     [Header("Move Variables")]
-    public float moveAmount = 1;
-    public float moveTimeAmount = 1;
-    
-    
+    public float moveSpeed = 6f;
+    public float moveMultiplier = 10f;
+
+    float horizontalMovement, verticalMovement;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+    float rbDrag = 6f;
+
+    [Header("Rotation Variables")]
+    public float sensX;
+    public float sensY;
+
+    float mouseX, mouseY;
+    float rotMultiplier = 0.1f;
+    float xRot, yRot;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Inputs();
+        controlDrag();
+        rotatePlayer();
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
     }
 
     private void FixedUpdate()
@@ -23,35 +54,41 @@ public class PlayerMovementController : MonoBehaviour
         movement();
     }
 
+
+    void Inputs()
+    {
+        //movement
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+
+        //rotation
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRot += mouseX * sensX * rotMultiplier;
+        xRot -= mouseY * sensY * rotMultiplier;
+
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+    }
+
     void movement()
     {
-        if(Input.GetKey(KeyCode.E))
-        {
-            this.transform.DOMoveY(transform.position.y + moveAmount, moveTimeAmount);
+        rb.AddForce(moveDirection.normalized * moveSpeed * moveMultiplier, ForceMode.Acceleration);
 
-        }
-        else if(Input.GetKey(KeyCode.Q))
-        {
-            this.transform.DOMoveY(transform.position.y - moveAmount, moveTimeAmount);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.DOMoveX(transform.position.x - moveAmount, moveTimeAmount);
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            this.transform.DOMoveX(transform.position.x + moveAmount, moveTimeAmount);
-        }
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.DOMoveZ(transform.position.z + moveAmount, moveTimeAmount);
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            this.transform.DOMoveZ(transform.position.z - moveAmount, moveTimeAmount);
-        }
     }
+
+    void controlDrag()
+    {
+        rb.drag = rbDrag;
+    }
+
+
+    void rotatePlayer()
+    {
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+    }
+
 
 }
